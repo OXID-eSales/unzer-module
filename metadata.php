1e1ceb70421f169c87a3a50b9d110486134a0ccb
Keywan Ghadami <keywan.ghadami@oxid-esales.com>
2021-10-26 09:15:14 +0200

    inital files and github action template from password policy module

---
+++ metadata.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.1';
+
+/**
+ * Module information.
+ */
+$aModule = [
+    'id' => 'osc-unzer',
+    'title' => 'Unzer Payment Modul für OXID',
+    'description' => [
+        'de' => '',
+        'en' => '',
+    ],
+    'thumbnail'    => 'logo.svg',
+    'version' => '1.0.0',
+    'author' => 'OXID eSales AG',
+    'url' => 'https://www.oxid-esales.com',
+    'email' => 'info@oxid-esales.com',
+    'extend' => [
+    ],
+    'controllers' => [
+    ],
+    'templates' => [
+    ],
+    'blocks' => [
+    ],
+    'settings' => [
+    ],
+    'events' => [
+    ],
+];
