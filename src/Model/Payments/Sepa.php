22e7f420ea0f76ca3fb9c7f7a39df7c96dd103d1
Mario Lorenz <mario.lorenz@oxid-esales.com>
2021-11-16 19:45:52 +0100

    fix: Error in Log:
    Object of class OxidSolutionCatalysts\Unzer\Model\Payment could not be converted to string

--- src/Model/Payments/Sepa.php
+++ src/Model/Payments/Sepa.php
@@ -39,7 +39,7 @@ class Sepa extends UnzerPayment
     {
         $oPayment = oxNew(Payment::class);
         $oPayment->load($oxpaymentid);
-        $this->$oPayment = $oPayment;
+        $this->oPayment = $oPayment;
     }
 
     /**
