33f339506c8e8a421d306f59e3f4ce9cbe6f0b36
Anton Fedurtsya <anton@fedurtsya.com>
2021-12-01 17:40:55 +0200

    Use ModuleSettings service in ViewConfig

---
+++ Tests/Integration/Core/ViewConfigTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace OxidSolutionCatalysts\Unzer\Tests\Integration\Controller;
+
+use OxidEsales\Eshop\Core\Registry;
+use OxidEsales\EshopCommunity\Internal\Container\ContainerFactory;
+use OxidEsales\EshopCommunity\Internal\Framework\Module\Configuration\Bridge\ModuleSettingBridgeInterface;
+use OxidEsales\TestingLibrary\UnitTestCase;
+use OxidSolutionCatalysts\Unzer\Module;
+use OxidSolutionCatalysts\Unzer\Service\ModuleSettings;
+
+class ViewConfigTest extends UnitTestCase
+{
+    public function testModuleSettings()
+    {
+        $di = ContainerFactory::getInstance()->getContainer();
+        $bridge = $di->get(ModuleSettingBridgeInterface::class);
+        $bridge->save('UnzerSystemMode', 1, Module::MODULE_ID);
+        $bridge->save('production-UnzerPublicKey', 'publickey', Module::MODULE_ID);
+        $bridge->save('production-UnzerPrivateKey', 'privatekey', Module::MODULE_ID);
+        $bridge->save('production-UnzerApiKey', 'apikey', Module::MODULE_ID);
+
+        /** @var \OxidSolutionCatalysts\Unzer\Core\ViewConfig $viewConfig */
+        $viewConfig = Registry::get(\OxidEsales\Eshop\Core\ViewConfig::class);
+        $this->assertSame(ModuleSettings::SYSTEM_MODE_PRODUCTION, $viewConfig->getUnzerSystemMode());
+        $this->assertSame('publickey', $viewConfig->getUnzerPubKey());
+        $this->assertSame('privatekey', $viewConfig->getUnzerPrivKey());
+    }
+}
