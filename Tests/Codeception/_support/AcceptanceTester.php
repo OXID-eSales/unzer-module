7f3afe451eec1cb4432655c8f3e2785970a68bbd
Anton Fedurtsya <anton@fedurtsya.com>
2021-12-03 16:15:25 +0200

    Prepare clean structure for Codeception tests

---
+++ Tests/Codeception/_support/AcceptanceTester.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace OxidSolutionCatalysts\Unzer\Tests\Codeception;
+
+/**
+ * Inherited Methods
+ *
+ * @method void wantToTest($text)
+ * @method void wantTo($text)
+ * @method void execute($callable)
+ * @method void expectTo($prediction)
+ * @method void expect($prediction)
+ * @method void amGoingTo($argumentation)
+ * @method void am($role)
+ * @method void lookForwardTo($achieveValue)
+ * @method void comment($description)
+ * @method void pause()
+ *
+ * @SuppressWarnings(PHPMD)
+ */
+class AcceptanceTester extends \Codeception\Actor
+{
+    use _generated\AcceptanceTesterActions;
+
+    /**
+     * Define custom actions here
+     */
+
+    public function saveShopConfVar($sVarType, $sVarName, $sVarVal, $sShopId = null, $sModule = '')
+    {
+        $config = \OxidEsales\Eshop\Core\Registry::getConfig();
+        $config->saveShopConfVar($sVarType, $sVarName, $sVarVal, $sShopId, $sModule);
+    }
+}
