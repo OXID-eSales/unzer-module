6fba05d6a922f607e24388348ef5c2cf8e991634
Dominik Adrian <d.adrian@dixeno.de>
2022-01-28 14:15:40 +0100

    unzer - 98,99,100

--- src/Controller/Admin/ModuleConfiguration.php
+++ src/Controller/Admin/ModuleConfiguration.php
@@ -20,6 +20,7 @@ class ModuleConfiguration extends ModuleConfiguration_parent
     use ServiceContainer;
 
     protected $translator;
+    protected $_sModuleId;
 
     /**
      * @inheritDoc
@@ -37,7 +38,7 @@ class ModuleConfiguration extends ModuleConfiguration_parent
     {
         parent::render();
 
-        if ($this->_sEditObjectId == Module::MODULE_ID) {
+        if ($this->_sModuleId == Module::MODULE_ID) {
             try {
                 $pubKey = $this->getServiceFromContainer(ModuleSettings::class)->getShopPublicKey();
                 $privKey = $this->getServiceFromContainer(ModuleSettings::class)->getShopPrivateKey();
