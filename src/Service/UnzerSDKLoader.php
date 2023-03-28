d9dec6c861fd1b2683c205a44581a4a55bd51df6
Lars Stegelitz <lars.stegelitz@oxid-esales.com>
2023-03-28 17:01:36 +0200

    added functionality to init SDK with a specific key

--- src/Service/UnzerSDKLoader.php
+++ src/Service/UnzerSDKLoader.php
@@ -10,6 +10,7 @@ namespace OxidSolutionCatalysts\Unzer\Service;
 use OxidEsales\Eshop\Application\Model\Basket;
 use OxidEsales\Eshop\Core\Registry;
 use OxidEsales\Eshop\Core\Session;
+use OxidEsales\Eshop\Core\DatabaseProvider;
 use OxidSolutionCatalysts\Unzer\Core\UnzerDefinitions;
 use UnzerSDK\Unzer;
 
@@ -45,20 +46,16 @@ class UnzerSDKLoader
     }
 
     /**
+     * @param string $customerType
+     * @param string $currency
      * @return Unzer
      */
-    public function getUnzerSDK(): Unzer
+    public function getUnzerSDK(string $customerType = '', string $currency = ''): Unzer
     {
-        $key = $this->moduleSettings->getShopPrivateKey();
-        $basket = $this->session->getBasket();
-        if ($basket instanceof Basket && $basket->getPaymentId() === UnzerDefinitions::INVOICE_UNZER_PAYMENT_ID) {
-            $request = Registry::getRequest();
-            /** @var string $customerType */
-            $customerType = $request->getRequestParameter('unzer_customer_type', '');
-
-            $key = $this->moduleSettings->getShopPrivateKeyInvoice($customerType);
+        if ($customerType != '' && $currency != '') {
+            return $this->getUnzerSDKbyCustomerTypeAndCurrency($customerType, $currency);
         }
-
+        $key = $this->moduleSettings->getShopPrivateKey();
         $sdk = oxNew(Unzer::class, $key);
 
         if ($this->moduleSettings->isDebugMode()) {
@@ -66,5 +63,57 @@ class UnzerSDKLoader
         }
 
         return $sdk;
+    }
+
+    /**
+     * Will return a Unzer SDK object using a specific key, depending on $customerType and $currency.
+     * Relevant for PaylaterInvoice. If $customerType or $currency is empty, the regular key is used.
+     * @param string $customerType
+     * @param string $currency
+     * @return Unzer
+     */
+    public function getUnzerSDKbyCustomerTypeAndCurrency(string $customerType, string $currency): Unzer
+    {
+        if ($customerType == '' || $currency == '') {
+            return $this->getUnzerSDK();
+        }
+
+        $key = $this->moduleSettings->getShopPrivateKeyInvoiceByCustomerTypeAndCurrency($customerType, $currency);
+        $sdk = oxNew(Unzer::class, $key);
+        if ($this->moduleSettings->isDebugMode()) {
+            $sdk->setDebugMode(true)->setDebugHandler($this->debugHandler);
+        }
+        return $sdk;
+    }
+
+    /**
+     * Initialize UnzerSDK from a payment id
+     * @param string $sPaymentId
+     * @return Unzer
+     */
+    public function getUnzerSDKbyPaymentType(string $sPaymentId): Unzer
+    {
+        $oDB = DatabaseProvider::getDb(DatabaseProvider::FETCH_MODE_ASSOC);
+        $row = $oDB->getRow("SELECT u.CURRENCY, o.OXDELCOMPANY, o.OXBILLCOMPANY, o.OXPAYMENTTYPE 
+                            FROM oscunzertransaction u 
+                            LEFT JOIN oxorder o ON u.OXORDERID = o.OXID
+                            WHERE u.TYPEID = :typeid 
+                            ORDER BY u.OXTIMESTAMP DESC LIMIT 1", [':typeid' => $sPaymentId]);
+
+        $customerType = '';
+        $currency = '';
+        if ($row) {
+            $currency = $row['CURRENCY'];
+            $paymentType = $row['OXPAYMENTTYPE'];
+            if ($paymentType == UnzerDefinitions::INVOICE_UNZER_PAYMENT_ID) {
+                if (empty($row['OXDELCOMPANY']) && empty($row['OXBILLCOMPANY'])) {
+                    $customerType = 'B2C';
+                }
+                else {
+                    $customerType = 'B2B';
+                }
+            }
+        }
+        return $this->getUnzerSDK($customerType, $currency);
     }
 }
