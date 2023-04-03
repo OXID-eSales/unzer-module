1765d1b32294a3c049bc879a5c1e09a554190ea4
Michael Walter <michael.walter@oxid-esales.com>
2023-04-03 14:04:47 +0200

    fixed codeception added "abort running workflows after another push"

--- tests/Codeception/Acceptance/_bootstrap.php
+++ tests/Codeception/Acceptance/_bootstrap.php
@@ -8,5 +8,8 @@
 declare(strict_types=1);
 
 // This is acceptance bootstrap
+use Codeception\Util\Autoload;
+
+Autoload::addNamespace('OxidSolutionCatalysts\Unzer\Tests\Codeception\Acceptance', __DIR__);
 $helper = new \OxidEsales\Codeception\Module\FixturesHelper();
 $helper->loadRuntimeFixtures(__DIR__ . '/../_data/fixtures.php');
