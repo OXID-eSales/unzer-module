4d4200d0a7c42efb1adbec444078fb1ed6caa070
Lars Stegelitz <lars.stegelitz@oxid-esales.com>
2023-04-06 20:01:00 +0200

    UNZER-166 paylater invoice B2C EUR test

--- Tests/Codeception/Acceptance/PaylaterInvoiceCest.php
+++ Tests/Codeception/Acceptance/PaylaterInvoiceCest.php
@@ -26,29 +26,65 @@ final class PaylaterInvoiceCest extends BaseCest
         return ['oscunzer_invoice'];
     }
 
-    protected function handleB2CElements(AcceptanceTester $I)
+    protected function fillB2Cdata(AcceptanceTester $I)
     {
         $consentCheckbox = Locator::firstElement('.unzerUI .checkbox');
+        $I->wait(2);
         $I->click($consentCheckbox);
         // use birthdate 10.10.1990
+        $I->wait(2);
         $I->fillField('#birthdate_day', 10);
         $monthPicker = Locator::find('button', ['data-id' => 'birthdate_month']);
         $I->click($monthPicker);
-        $monthElem = Locator::find('li', ['data-original-index' => 10]);
-        $I->click($monthElem);
+        $I->wait(2);
+        $I->click(['css' => "#consumer_common li[data-original-index='10']"]);
+        $I->wait(2);
         $I->fillField('#birthdate_year', 1990);
+    }
+
+    protected function fillB2Bdata(AcceptanceTester $I)
+    {
+        // same as B2C
+        $this->fillB2Cdata($I);
+        // B2B specific
+        $companyTypeSelect = Locator::find('button', ['data-id' => 'unzer_company_form']);
+        $I->wait(2);
+        $I->click($companyTypeSelect);
+        $I->wait(2);
+        $I->click(['css' => "#consumer_b2b li[data-original-index='1']"]);
     }
 
     /**
      * @param AcceptanceTester $I
-     * @group PaylaterInvoicePaymentTest
+     * @group PaylaterInvoiceB2CEUR
      */
     public function checkPaymentB2CEURWorks(AcceptanceTester $I)
     {
         $I->wantToTest('PaylaterInvoice B2C EUR payment works');
         $this->_initializeSecuredTest();
         $orderPage = $this->_choosePayment($this->invoicePaymentLabel);
-        $this->handleB2CElements($I);
+        $this->fillB2Cdata($I);
+        $orderPage->submitOrder();
+
+        $this->_checkSuccessfulPayment();
+    }
+
+    /**
+     * @param AcceptanceTester $I
+     * @group PaylaterInvoiceB2BEUR
+     */
+    public function checkPaymentB2BEURWorks(AcceptanceTester $I)
+    {
+        $I->wantToTest('PaylaterInvoice B2B EUR payment works');
+        // if user has a company it will trigger B2B mode
+        $I->updateInDatabase(
+            'oxuser',
+            ['oxcompany' => 'ACME'],
+            ['oxid' => 'unzersecureuser']
+        );
+        $this->_initializeSecuredTest();
+        $orderPage = $this->_choosePayment($this->invoicePaymentLabel);
+        $this->fillB2Bdata($I);
         $orderPage->submitOrder();
 
         $this->_checkSuccessfulPayment();
