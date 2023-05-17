7325b79dec9f5d9145e7b8890c1f44d8232e47c7
Lars Stegelitz <lars.stegelitz@oxid-esales.com>
2023-05-17 12:49:18 +0200

    UNZER rectify german transalation for "paylater invoice"

--- translations/de/oscunzer_lang.php
+++ translations/de/oscunzer_lang.php
@@ -24,11 +24,11 @@ $aLang = [
     'OSCUNZER_CONSUMER_TARGET_B2C' => 'Privatperson',
 
     'OSCUNZER_COMPANY_FORM' => 'Unternehmens-<br>form',
-    'OSCUNZER_COMPANY_FORM_authority' => 'Authority (Staatliches Unternehmen?)',
-    'OSCUNZER_COMPANY_FORM_association' => 'Association (Organisation?)',
-    'OSCUNZER_COMPANY_FORM_sole' => 'Sole (Einzelunternehmen?)',
-    'OSCUNZER_COMPANY_FORM_company' => 'Company (Firma?)',
-    'OSCUNZER_COMPANY_FORM_other' => 'Other (andere?)',
+    'OSCUNZER_COMPANY_FORM_authority' => 'Behörde',
+    'OSCUNZER_COMPANY_FORM_association' => 'Interessenverband',
+    'OSCUNZER_COMPANY_FORM_sole' => 'Einzelunternehmen',
+    'OSCUNZER_COMPANY_FORM_company' => 'Betrieb',
+    'OSCUNZER_COMPANY_FORM_other' => 'Sonstige',
 
     // Invoice Secured B2B
     'OSCUNZER_COMMERCIAL_SECTOR' => 'Wirtschaftszweig',
