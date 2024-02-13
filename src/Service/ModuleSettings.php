2667aa710b1862af119922666d39cbcc40401855
Danny <danny.zimmer@maexware-solutions.de>
2024-02-13 08:48:42 +0100

    UNZER-291 Fix Eligibility

--- src/Service/ModuleSettings.php
+++ src/Service/ModuleSettings.php
@@ -532,7 +532,7 @@ class ModuleSettings
     {
         return (
         ($this->isB2CInstallmentEligibility() &&
-            $this->hasWebhookConfiguration('b2ceurinstallment') &&
+            $this->hasWebhookConfiguration('b2ceurinstallment') ||
             $this->hasWebhookConfiguration('b2cchfinstallment'))
         );
     }
