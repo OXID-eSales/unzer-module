2b06df3b0f7fd14f518064fc2c294e62f1a576f6
Mario Lorenz <mario.lorenz@oxid-esales.com>
2024-04-02 13:10:44 +0200

    prepare CHANGELOG

--- metadata.php
+++ metadata.php
@@ -52,7 +52,7 @@ $aModule = [
             </ul>',
     ],
     'thumbnail' => 'logo.svg',
-    'version' => '2.2.0-rc.3',
+    'version' => '1.2.0-rc.2',
     'author' => 'OXID eSales AG',
     'url' => 'https://www.oxid-esales.com',
     'email' => 'info@oxid-esales.com',
