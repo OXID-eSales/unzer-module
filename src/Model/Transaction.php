47e434b88a3655c1a8432baf0061513f95d7509d
Mario Lorenz <mario.lorenz@oxid-esales.com>
2024-06-25 15:10:10 +0200

    phpStan / phpMD / phpStan

--- src/Model/Transaction.php
+++ src/Model/Transaction.php
@@ -168,6 +168,7 @@ class Transaction extends BaseModel
 
     public function setPaymentTypeId(?string $status): void
     {
+        $status = $status ?: '';
         $this->_setFieldData('PAYMENTTYPEID', $status);
     }
 }
