dd621208291c1d4ae53475025111cfd4143a1b07
René Gust <rene.gust@oxid-esales.com>
2024-07-22 13:19:23 +0200

    UNZER-457 refactor sending recurrence type oneclick

--- src/PaymentExtensions/UnzerPayment.php
+++ src/PaymentExtensions/UnzerPayment.php
@@ -20,6 +20,7 @@ use OxidSolutionCatalysts\Unzer\Service\Translator;
 use OxidSolutionCatalysts\Unzer\Service\Unzer as UnzerService;
 use OxidSolutionCatalysts\Unzer\Service\UnzerSDKLoader;
 use OxidSolutionCatalysts\Unzer\Traits\ServiceContainer;
+use UnzerSDK\Constants\RecurrenceTypes;
 use UnzerSDK\Exceptions\UnzerApiException;
 use UnzerSDK\Resources\Basket as UnzerResourceBasket;
 use UnzerSDK\Resources\Customer;
@@ -156,12 +157,11 @@ abstract class UnzerPayment
             $userModel->save();
         }
 
-        if ($paymentType instanceof UnzerSDKPaymentTypeCard || $paymentType instanceof Paypal) {
-            $savePayment = $request->getRequestParameter('oscunzersavepayment');
-            if ($savePayment && $this->existsInSavedPaymentsList($userModel)) {
-                $savePayment = false;
-            }
-            $session->setVariable('oscunzersavepayment', $savePayment);
+        if ($this->isSafePaymentClickedByUserInRequest($paymentType)) {
+            $session->setVariable(
+                'oscunzersavepayment',
+                $this->existsInSavedPaymentsList($userModel) ? $request->getRequestParameter('oscunzersavepayment') : false
+            );
         }
 
         if ($userModel->getId()) {
@@ -324,10 +324,11 @@ abstract class UnzerPayment
         if ($currentPayment) {
             $currentPaymentType = $currentPayment->getPaymentType();
             foreach ($savedUserPayments as $savedPayment) {
-                if ($currentPaymentType instanceof UnzerSDKPaymentTypeCard) {
-                    if ($this->areCardsEqual($currentPaymentType, $savedPayment)) {
+                if (
+                    $currentPaymentType instanceof UnzerSDKPaymentTypeCard
+                    && $this->areCardsEqual($currentPaymentType, $savedPayment)
+                ) {
                         return true;
-                    }
                 }
                 if (
                     ($currentPaymentType instanceof Paypal) &&
@@ -385,7 +386,11 @@ abstract class UnzerPayment
             $customer,
             $this->unzerOrderId,
             $this->unzerService->getShopMetadata($this->paymentMethod),
-            $uzrBasket
+            $uzrBasket,
+            null,
+            null,
+            null,
+            $this->isSafePaymentClickedByUserInRequest($paymentType) ? RecurrenceTypes::ONE_CLICK : null
         );
     }
 
@@ -424,7 +429,12 @@ abstract class UnzerPayment
             true,
             null,
             null,
-            \UnzerSDK\Constants\RecurrenceTypes::ONE_CLICK
+            RecurrenceTypes::ONE_CLICK
         );
+    }
+
+    private function isSafePaymentClickedByUserInRequest(BasePaymentType $paymentType) {
+        return ($paymentType instanceof UnzerSDKPaymentTypeCard || $paymentType instanceof Paypal)
+            && Registry::getRequest()->getRequestParameter('oscunzersavepayment');
     }
 }
