c1f11091b5143958545fa3b769ff27519c5e755f
René Gust <rene.gust@oxid-esales.com>
2024-08-21 15:31:50 +0200

    UNZER-646 fix sql

--- Tests/Unit/Service/SavedPayment/SQL/LoadQueries.php
+++ Tests/Unit/Service/SavedPayment/SQL/LoadQueries.php
@@ -5,12 +5,12 @@ namespace OxidSolutionCatalysts\Unzer\Tests\Unit\Service\SavedPayment\SQL;
 class LoadQueries
 {
     public const LOAD_TRANSACTIONS_SQL = 'SELECT transactionAfterOrder.OXORDERID, transactionBeforeOrder.OXID,
-            transactionBeforeOrder.PAYMENTTYPEID, transactionBeforeOrder.CURRENCY,
+            transactionAfterOrder.PAYMENTTYPEID, transactionBeforeOrder.CURRENCY,
             transactionBeforeOrder.CUSTOMERTYPE, oxorder.OXPAYMENTTYPE,
             transactionBeforeOrder.OXACTIONDATE, transactionBeforeOrder.SAVEPAYMENT
             FROM oscunzertransaction as transactionBeforeOrder
                 LEFT JOIN oscunzertransaction as transactionAfterOrder
-                    on transactionBeforeOrder.PAYMENTTYPEID = transactionAfterOrder.PAYMENTTYPEID
+                    on transactionBeforeOrder.SHORTID = transactionAfterOrder.SHORTID
                 INNER JOIN oxorder ON transactionAfterOrder.oxorderid = oxorder.OXID
             WHERE transactionBeforeOrder.OXUSERID = :oxuserid 
               AND transactionAfterOrder.PAYMENTTYPEID IS NOT NULL 
@@ -19,7 +19,7 @@ class LoadQueries
     public const LOAD_TRANSACTIONS_BY_USER_ID_SQL = "SELECT transactionBeforeOrder.OXID 
             FROM oscunzertransaction as transactionBeforeOrder 
             INNER JOIN oscunzertransaction as transactionAfterOrder
-                ON transactionBeforeOrder.PAYMENTTYPEID = transactionAfterOrder.PAYMENTTYPEID
+                ON transactionBeforeOrder.SHORTID = transactionAfterOrder.SHORTID
             WHERE transactionAfterOrder.SAVEPAYMENTUSERID = :savedPaymentUserId
             AND transactionBeforeOrder.SAVEPAYMENT = 1";
 }
