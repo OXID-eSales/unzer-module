1b7085a955f176055df4da775be59dbd9eebaccf
Mario Lorenz <mario.lorenz@oxid-esales.com>
2025-07-10 14:35:04 +0200

    fix compatibility with Core-function

--- src/Controller/Admin/OrderMain.php
+++ src/Controller/Admin/OrderMain.php
@@ -19,7 +19,7 @@ class OrderMain extends OrderMain_parent
      * @throws \UnzerSDK\Exceptions\UnzerApiException
      * @throws \OxidEsales\Eshop\Core\Exception\DatabaseConnectionException
      */
-    protected function onOrderSend(): void
+    protected function onOrderSend()
     {
         $sOxid = $this->getEditObjectId();
         $oOrder = oxNew(Order::class);
